7b346a4e4dc74e1f68d219a6720a2435cac2fff9
connolly <Morgan Connolly>
2023-06-20 15:23:47 -0700

    define define_shortcut function

--- shared.sh
+++ shared.sh
@@ -1,7 +1,13 @@
+define_shortcut() {
+    declare $1=$2
+    alias $1="cd $2"
+}
+
 config_files=(
     git_functions.sh
     uber_variables.sh
     uber_functions.sh
+    life_variables.sh
 )
 for f in $config_files; do
     source ${zshrcs}/$f
--- uber_variables.sh
+++ uber_variables.sh
@@ -1,8 +1,14 @@
-cli=${HOME}/marmaray/marmaray-uber-tools/src/main/cli
-alias cli="cd $cli"
+define_shortcut cli ${HOME}/marmaray/marmaray-uber-tools/src/main/cli
+
+#cli=${HOME}/marmaray/marmaray-uber-tools/src/main/cli
+#alias cli="cd $cli"
 
 fievel=${HOME}/fievel
 alias fievel="cd $fievel"
 
 watchtower=${fievel}/data/data-ingestion/watchtower-service
 alias watchtower="cd ${watchtower}"
+
+allocation=/users/connolly/marmaray/marmaray-uber-tools/src/main/java/com/uber/marmaray/tools/hoover/allocation
+alias allocation="cd ${allocation}"
+
