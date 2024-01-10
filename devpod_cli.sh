01841547c4959b435538478c29f0674967678c87
connolly <Morgan Connolly>
2024-01-10 15:17:10 -0800

    pass arguments to dssh

--- devpod_cli.sh
+++ devpod_cli.sh
@@ -2,10 +2,12 @@
 dssh() {
     ssh-add $HOME/.ssh/id_ecdsa
     set -x
-    devpod wait $1 && ssh -A connolly-$1.devpod-us-or
+    devpod wait $1 && ssh -A connolly-$1.devpod-us-or "${@:2}"
     unset -x
 }
 
 fireup () {
     devpod create $1 || devpod start $1 && dssh $1
 }
+
+
