871de7af362322814c2b808fa7af9b97dcbe8157
connolly <connolly@uber.com>
2024-04-12 22:31:49 +0000

    resolve merge conflicts

--- uber_variables.sh
+++ uber_variables.sh
@@ -10,7 +10,7 @@ define_shortcut watchtower ${fievel}/data/data-ingestion/watchtower-service
 watchtower=${fievel}/data/data-ingestion/watchtower-service
 
 define_shortcut pcp ${HOME}/uber-one/piper-core-pipelines
-define_shortcut piper ${HOME}/piper
+define_shortcut piper_tools ${pcp}/piper_tools
 define_shortcut marmaray ${HOME}/marmaray
 define_shortcut cloudlake_tools ${cli}/cloudlake_tools
 cloudlake_tools=${cli}/cloudlake_tools
